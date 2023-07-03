2b5780bb2752bb5189c89991cda66dda21d19f96
kimia varasteh <kvar923@aucklanduni.ac.nz>
2023-07-03 18:00:41 +1200

    js file added for functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+const inputBox = document.querySelector(".input-box"); 
+const listContainer = document.querySelector(".list-container"); 
+
+function addTask() {
+  if (inputBox.value == "") {
+    // user must recieve an alert if there is no input
+    alert("you must enter a task"); 
+  }
+
+  else {
+    // creating a new list row
+    let li = document.createElement("li"); 
+    // the value of it should be initialised to the input value
+    li.innerHTML = inputBox.value; 
+    // this li will be displayed below listContainer 
+    listContainer.appendChild(li);
+    let span = document.createElement("span"); 
+    //cross icon
+    span.innerHTML = "\u00d7"; 
+    li.appendChild(span); 
+
+  }
+  inputBox.value = ""; 
+}
